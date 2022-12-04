e8ccf16e4405bcf0ea812ecca6f234d7e97b3283
Per Ingemarsson <pi222cp@student.lnu.se>
2022-12-04 22:21:33 +0100

    Add day 4

---
+++ day 4/day_4_part1.py
@@ -0,0 +1,10 @@
+with open("day 4\\data.txt") as file:
+    lines = file.readlines()
+sum = 0
+for line in lines:
+    first, second = line.split(",")
+    first_start, first_end = first.split("-")
+    second_start, second_end = second.split("-")
+    if int(first_start) <= int(second_start) and int(first_end) >= int(second_end): sum += 1
+    elif int(second_start) <= int(first_start) and int(second_end) >= int(first_end): sum += 1
+print(sum)
